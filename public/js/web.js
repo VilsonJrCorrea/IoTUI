d2f70ead21aff81b6646655242e8b74b6d697162
Vilson Júnior <vilsonjrcorrea@gmail.com>
2018-12-16 16:10:01 -0300

    Com duas lampadas

--- public/js/web.js
+++ public/js/web.js
@@ -1,10 +1,25 @@
 // var firebase = require("firebase/app");
 // require("firebase/auth");
 var firebase = require("firebase");
+var lampadaAtual = 'lampadaOne';
+
+
+function navOneClicada() {
+    document.getElementById("navOne").style.background = "white";
+    document.getElementById("navTwo").style.background = "none";
+    lampadaAtual = 'lampadaOne';
+}
+
+function navTwoClicada() {
+    document.getElementById("navTwo").style.background = "white";
+    document.getElementById("navOne").style.background = "none";
+    lampadaAtual = 'lampadaTwo';
+}
 
 function changeLabel() {
     var barraAux = barra = document.getElementById("barra").value;
-    firebase.database().ref('lampada').set(barra);
+    console.log(lampadaAtual);
+    firebase.database().ref(lampadaAtual).set(barra);
     // var barraAux = document.getElementById("barra").value;
     let porcentagem = Math.round((100 * barraAux) / 255);
     // if (barra == 0) {
@@ -13,4 +28,3 @@ function changeLabel() {
     document.getElementById("resposta").innerHTML = "Sua lâmpada está com " + porcentagem + "% de luminosidade";
     // }
 }
-
