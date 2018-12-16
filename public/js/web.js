736bff8926a4439183fd16217ffd77ee1f1b3108
Vilson Júnior <vilsonjrcorrea@gmail.com>
2018-12-16 17:23:24 -0300

    Alterações barra

--- public/js/web.js
+++ public/js/web.js
@@ -2,35 +2,45 @@
 // require("firebase/auth");
 var firebase = require("firebase");
 var lampadaAtual = 'lampadaOne';
+var admin = require("firebase-admin");
+
 
 function navOneClicada() {
     document.getElementById("navOne").style.background = "white";
     document.getElementById("navTwo").style.background = "none";
     lampadaAtual = 'lampadaOne';
+    firebase.database().ref(lampadaAtual).on('value', function (snapshot) {
+        let valor = snapshot.val();
+        document.getElementById("barra").value = valor;
+        let porcentagem = Math.round((100 * valor) / 255);
+        document.getElementById("resposta").innerHTML = "Sua lâmpada está com " + porcentagem + "% de luminosidade";
+    });
 }
 
 function navTwoClicada() {
     document.getElementById("navTwo").style.background = "white";
     document.getElementById("navOne").style.background = "none";
     lampadaAtual = 'lampadaTwo';
+    firebase.database().ref(lampadaAtual).on('value', function (snapshot) {
+        let valor = snapshot.val();
+        document.getElementById("barra").value = valor;
+        let porcentagem = Math.round((100 * valor) / 255);
+        document.getElementById("resposta").innerHTML = "Sua lâmpada está com " + porcentagem + "% de luminosidade";
+    });
 }
 
 function changeLabel() {
     var barraAux = barra = document.getElementById("barra").value;
     console.log(lampadaAtual);
-
     firebase.database().ref(lampadaAtual).set(barra);
-
-    // var barraAux = document.getElementById("barra").value;
     let porcentagem = Math.round((100 * barraAux) / 255);
-    // if (barra == 0) {
-    //     document.getElementById("resposta").innerHTML = "Sua lâmpada está apagada";
-    // } else {
     document.getElementById("resposta").innerHTML = "Sua lâmpada está com " + porcentagem + "% de luminosidade";
-    // }
 }
 
-var barra = document.getElementById("barra").value;
-firebase.database().ref(lampadaAtual).set(barra);
-console.log(firebase.database().ref(lampadaAtual));
 
+firebase.database().ref(lampadaAtual).on('value', function (snapshot) {
+    console.log(snapshot.val());
+});
+firebase.database().ref.on("child_changed", function (snapshot) {
+    console.log("---> " + snapshot.val());
+});
